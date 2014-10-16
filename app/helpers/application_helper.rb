f30cd8af97e4b8c356928f5e0a0fa60c173d8305
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2014-10-16 10:11:36 -0600

    Fixed some formatting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,11 @@ module ApplicationHelper
 
   def search_html form, label, label_text, sub_label, path, value
     haml_tag(:div, :class => "search") do
-      haml_concat (form.label label, label_text)
-      haml_concat (form.hidden_field label, :class => 'search_id')
-      haml_concat (form.fields_for sub_label do |t|
+      haml_concat(form.label label, label_text)
+      haml_concat(form.hidden_field label, :class => 'search_id')
+      haml_concat((form.fields_for sub_label do |t|
         form.text_field sub_label, :class => 'search_field', :data => {:path => path}, :value => value
-      end)
+      end))
       haml_tag(:div, :class => "search_results") do
       end
     end
