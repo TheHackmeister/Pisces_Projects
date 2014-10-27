ae7f0642a0b80a8ebc5570e021c219bc92635051
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2014-10-27 12:41:29 -0600

    Forgot one.

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,4 +2,8 @@ class Step < ActiveRecord::Base
   belongs_to :step_status
   belongs_to :project
   validates :action, :presence => true
+  
+  after_initialize {
+    self.step_status ||= StepStatus.new
+  }
 end
