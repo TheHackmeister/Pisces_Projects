17b39d28f3afd3cd2dea081c7f18c9cdb14c5eb1
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2014-11-25 14:01:01 -0700

    Fixes a deprecation warning.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class PiscesDatabase < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection "pisces_database_production"
+  establish_connection :pisces_database_production
 end
 
 class Customer < PiscesDatabase
