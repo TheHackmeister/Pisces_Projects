f9fc853803c459e4b2e8020ca6d1a6c9acf87ad5
Spencer Hachmeister <spencerhachmeister@gmail.com>
2015-04-08 14:25:18 -0600

    Changed the name of the customer_name param. To search it is now customer_name_search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,9 @@
 class CustomersController < ApplicationController
   load_and_authorize_resource 
   def index
+    params[:customer_name_search] = params.delete :customer_name
 
-    @customers = Customer.filter(params.slice(:customer_name))
+    @customers = Customer.filter(params.slice(:customer_name_search))
     respond_to do |format|
       format.html
       format.json {
