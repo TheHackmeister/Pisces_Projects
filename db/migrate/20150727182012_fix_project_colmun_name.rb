310c1bbe26782dc0cdded9c5c2e8feb6e33ac8fb
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2015-07-28 13:38:15 -0600

    small mistake in fixing the column name.

--- db/migrate/20150727182012_fix_project_colmun_name.rb
+++ db/migrate/20150727182012_fix_project_colmun_name.rb
@@ -1,7 +1,7 @@
 class FixProjectColmunName < ActiveRecord::Migration
   def change
 		remove_index :project_links, :name => 'index_project_links_on_Project_id'
-		rename_column :project_links, :project_id, :project_ida
+		rename_column :project_links, :Project_id, :project_ida
 		rename_column :project_links, :project_ida, :project_id
 		add_index :project_links, ["project_id"], :name => 'index_project_links_on_project_id'
   end
