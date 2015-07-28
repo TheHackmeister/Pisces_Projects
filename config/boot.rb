c168cb9730dc42ffe1e439e8f908aad8a142e286
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2015-07-28 15:33:19 -0600

    Allows outside localhost access.
    Needed after 4.2 update.

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# This allows me to run a development env that is accessable from outside the localhost.
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge(Host: '0.0.0.0', Port: 8080)
+    end
+  end
+end
