38014bebac3823f17744c81b4e62af8a27c4a4b1
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2015-08-12 12:15:53 -0600

    Fixed controller specs

--- lib/templates/rspec/scaffold/controller_spec.rb
+++ lib/templates/rspec/scaffold/controller_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
+###################################################################
+# You must manual set a validation for create and update to work. #
+###################################################################
 
 # This assumes that we're using FactoryGirl
 # That there is a :user factory
@@ -6,15 +9,15 @@ require 'rails_helper'
 
 <% module_namespacing do -%>
 RSpec.describe <%= controller_class_name %>Controller, <%= type_metatag(:controller) %> do
-	include_context 'application' # Sets up user, admin, and <%= controller_class_name %>
+	include_context 'controllers' # Sets up user, admin, and <%= controller_class_name %>
 	
-	it_behaves_like 'an index page'
-	it_behaves_like 'a show page'
-	it_behaves_like 'an edit page'
-	it_behaves_like 'a new page'
-	it_behaves_like 'a create page', {:text => nil}
-	it_behaves_like 'an update page', {:text => nil}, {:text => "Example Text"}
-	it_behaves_like 'a delete page' 
+	it_behaves_like 'an index controller'
+	it_behaves_like 'a show controller'
+	it_behaves_like 'an edit controller'
+	it_behaves_like 'a new controller'
+	it_behaves_like 'a create controller', {:text => nil}
+	it_behaves_like 'an update controller', {:text => nil}, {:text => "Example Text"}
+	it_behaves_like 'a delete controller' 
 
 end
 <% end -%>
